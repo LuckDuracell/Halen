672517dcd307e8a0c129444231b111ae772be852
LuckDev <lukedru@yahoo.com>
2023-02-19 02:52:45 -0800

    Initial Commit

---
+++ Halen/HalenApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalenApp.swift
+//  Halen
+//
+//  Created by Luke Drushell on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct HalenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
